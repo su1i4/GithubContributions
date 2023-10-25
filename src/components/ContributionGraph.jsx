3791350b9baa09b346cb7a18c9b4f80f11ee7c91
Kamchybek <kamchybek@users-MacBook-Pro.local>
2023-10-26 00:32:14 +0600

    Received contributions from the server and saved them to the state.

--- src/components/ContributionGraph.jsx
+++ src/components/ContributionGraph.jsx
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { styled } from '@mui/material';
 
-const ContributionGraph = () => {
-  return <div>ContributionGraph</div>;
-};
+const Week = styled('div')`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Grid = styled('div')`
+  display: flex;
+`;
+
+function ContributionGraph() {
+  const [contributions, setContributions] = useState({});
+
+  useEffect(() => {
+    const getContributions = async () => {
+      try {
+        const { data } = await axios.get('https://dpg.gg/test/calendar.json');
+        setContributions(data);
+      } catch (error) {
+        throw new Error(error);
+      }
+    };
+
+    getContributions();
+  }, []);
+
+  return <Grid>test</Grid>;
+}
 
 export default ContributionGraph;
